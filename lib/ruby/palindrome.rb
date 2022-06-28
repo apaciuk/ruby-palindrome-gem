afade2abe062a885a0c08e019984780ab59841ca
Paul Anthony McGowan <xhostcom@gmail.com>
2022-06-28 05:50:05 +0100

    Upddate

--- lib/ruby/palindrome.rb
+++ lib/ruby/palindrome.rb
@@ -4,7 +4,7 @@ require_relative "palindrome/version"
 
 class String 
   def palindrome?
-   letters = scan(/[a-zA-Z]/).join.downcase
+   letters = scan(/[a-z]/i).join.downcase
    letters == letters.reverse
   end
 end
